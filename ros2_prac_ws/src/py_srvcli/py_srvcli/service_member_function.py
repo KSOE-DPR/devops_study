e7b883e94d32f18036e298f571294a263aeb5358
shl_nuc <gllass0838@gmail.com>
2023-08-31 13:40:15 +0900

    basic service and client file

---
+++ ros2_prac_ws/src/py_srvcli/py_srvcli/service_member_function.py
@@ -0,0 +1,34 @@
+from example_interfaces.srv import AddTwoInts
+from tutorial_interfaces.srv import CustomSrv
+import rclpy
+from rclpy.node import Node
+
+
+class MinimalService(Node):
+
+    def __init__(self):
+        super().__init__('minimal_service')
+        # self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
+        self.srv  = self.create_service(CustomSrv, 'custom_srv',self.CustomSrv_callback)
+    # def add_two_ints_callback(self, request, response):
+    #     response.sum = request.a + request.b
+    #     self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+    #     return response
+
+    def CustomSrv_callback(self, request, response):
+        response.sum = request.a + request.b + request.c
+        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+
+        return response
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_service = MinimalService()
+
+    rclpy.spin(minimal_service)
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
