99b53ecf144cfa7d710f536f5e745ec4e4f1c19f
ksron <kasrlmoojoo@gmail.com>
2023-09-01 10:23:16 +0900

    implemented a base function: fibonacci

--- my_math/my_math/fibonacci.py
+++ my_math/my_math/fibonacci.py
@@ -1,6 +1,18 @@
-def main():
-    print('Hi from my_math.')
+'''
+피보나치 수열 계산
+input: 정수 n
+output: 피보나치 수
 
+참고: 다음 함수에는 오류가 있음
+'''
+def fib(n):
+    if n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+def main():
+    print('Running Fibonacci')
 
 if __name__ == '__main__':
     main()
---
+++ my_math/test/test_fibonacci.py
@@ -0,0 +1,9 @@
+def test_always_passes():
+    assert True
+
+def test_always_fails():
+    assert False
+
+def test_fibonacci_general():
+    assert fib(7) == 13
+    assert fib(8) == 21
