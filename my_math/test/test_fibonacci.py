330804c463ebadec900c801dfbbe1de97afe3a07
hyeri <wlwhshyeri@gmail.com>
2023-09-04 15:43:34 +0900

    modify fib

--- my_math/test/test_fibonacci.py
+++ my_math/test/test_fibonacci.py
@@ -3,8 +3,8 @@ from my_math.fibonacci import fib
 def test_always_passes():
     assert True
 
-def test_always_fails():
-    assert False
+# def test_always_fails():
+#     assert False
 
 def test_fibonacci_general():
     assert fib(7) == 13
