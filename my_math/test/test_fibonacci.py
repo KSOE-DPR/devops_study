9641fe5fe560c286b2376a649b7417b90f199412
hyeri <wlwhshyeri@gmail.com>
2023-09-08 14:44:33 +0900

    edit test cases in test_fibonacci.py

--- my_math/test/test_fibonacci.py
+++ my_math/test/test_fibonacci.py
@@ -7,9 +7,15 @@ def test_always_passes():
 #     assert False
 
 def test_fibonacci_general():
-    assert fib(7) == 13
-    assert fib(8) == 21
-    assert fib(1) == 1
-    assert fib(-1) == -1
+    assert fib(-1) == 0
     assert fib(0) == 0
-    assert fib(2) == 1
+    assert fib(1) == 1
+    assert fib(2) == 2
+    assert fib(3) == 3
+    assert fib(4) == 5
+    assert fib(5) == 8
+    assert fib(6) == 13
+    assert fib(7) == 21
+    assert fib(8) == 34
+    assert fib(9) == 55
+
